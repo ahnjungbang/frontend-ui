2f9fa08ee99f6e1df3212f70c0970724ac6bc8c8
ahnjungbang <cadahn1@gmail.com>
2018-12-02 13:36:18 +0900

    슬라이드

---
+++ 02_imageSlider/js/type1.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+$items = $("slider ul li.item");
+
+var CurrentIndex = 1;
+var imgCount = $items.length;
+
+setInterval(function() {
+    if (CurrentIndex >= imgCount) {
+        CurrentIndex = 0;
+       }
+    // 1. 모든 이미지에 active 클래스를 제어한다.
+    $items.removeClass('active');
+    $items.eq(CurrentIndex++).addClass('active');
+    }, 1000);
+});
